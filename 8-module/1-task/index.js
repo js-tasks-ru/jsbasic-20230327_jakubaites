e16bc8f07d21a7a6f981d850f4c2f2ebcc1ef079
Jakubaites Valentin <Jakubaites@yandex.ru>
2023-04-30 13:46:00 +0300

    create updatePosition

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -14,13 +14,14 @@ export default class CartIcon {
   update(cart) {
     if (!cart.isEmpty()) {
       this.elem.classList.add('cart-icon_visible');
-
+      
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
           <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
         </div>`;
-
+      
+      this.elem.initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
       this.updatePosition();
 
       this.elem.classList.add('shake');
@@ -37,8 +38,44 @@ export default class CartIcon {
     document.addEventListener('scroll', () => this.updatePosition());
     window.addEventListener('resize', () => this.updatePosition());
   }
-
+  
+  
+  
   updatePosition() {
-    // ваш код ...
+    if(this.elem.offsetWidth == 0) return;
+    if (document.documentElement.clientWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+    }
+    // создаем прокрутку
+    
+    if (window.pageYOffset > this.elem.initialTopCoord) {
+      // плавающая корзина
+      let leftIndent = Math.min(
+        document.querySelector('.container').getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + 'px'
+      
+      Object.assign(this.elem.style, {
+        position: 'fixed',
+        top: '50px',
+        zIndex: 1e3,
+        right: '10px',
+        left: leftIndent
+      });
+
+    } else {
+      // корзина сверху
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+    }
   }
 }
